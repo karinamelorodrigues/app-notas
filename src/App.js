787a548df69a6fcd1a54b54a164ea7e237a885a7
Karina Rodrigues <karinamelorodrigues@gmail.com>
2021-11-29 09:52:31 -0300

    Adição de handler no formulário para salvar a categoria durante a criação da nota

--- src/App.js
+++ src/App.js
@@ -11,12 +11,12 @@ class App extends Component {
 
     this.state = {
       notas: [],
-      categorias: [],
+      categorias: ["Música", "Livros", "Trabalho"],
     };
   }
 
-  criarNota(titulo, texto) {
-    const novaNota = { titulo, texto };
+  criarNota(titulo, texto, categoria) {
+    const novaNota = { titulo, texto, categoria };
     const novoArrayNotas = [...this.state.notas, novaNota];
     const novoEstado = {
       notas: novoArrayNotas,
@@ -29,7 +29,6 @@ class App extends Component {
     let arrayNotas = this.state.notas;
     arrayNotas.splice(index, 1);
     this.setState({ notas: arrayNotas });
-    console.log("Deletar");
   }
 
   adicionarCategoria(valorCategoria) {
